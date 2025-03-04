488c59a354153ae2e02f26737eb24ee99a0d0017
hanyucd <1807003141@qq.com>
2025-03-04 18:09:36 +0800

    init: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    // port: 80,
+    // host: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://consult-api.itheima.net',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   }
+    // },
+  },
+})
